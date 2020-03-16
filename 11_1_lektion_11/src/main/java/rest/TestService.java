c113ac0f23897ace7824f0ddaaf280e735ce9a61
s174255 <Hfe93pzr>
2020-03-16 20:34:31 +0100

    added module 11_1_lektion_11 solution is in progress

---
+++ 11_1_lektion_11/src/main/java/rest/TestService.java
@@ -0,0 +1,14 @@
+package main.java.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Path("bye")
+public class TestService {
+
+    @GET
+    public String getBye(){
+        return "Test Service";
+    }
+}
