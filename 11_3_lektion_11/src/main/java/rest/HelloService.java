aa46ba43a4af24b64e9a19cb0c4a2fc721077721
s174255 <Hfe93pzr>
2020-03-22 13:36:45 +0100

    Added solution to 11_3

---
+++ 11_3_lektion_11/src/main/java/rest/HelloService.java
@@ -0,0 +1,20 @@
+package main.java.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Path("hello")
+public class HelloService {
+    @GET
+    public String sayHello(){
+        return "hello";
+    }
+
+    @POST
+    public String getName(String name){
+        return "Hello " + name;
+    }
+
+
+}
