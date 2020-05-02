c348dc1b6f774c793ffc666835f69c4254b9200b
olimikkelsen <runeogoli321>
2020-05-02 13:51:39 +0200

    almost working

---
+++ 11_4_lektion_11/src/main/java/data/TodoDAO.java
@@ -0,0 +1,55 @@
+package main.java.data;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoDAO {
+
+    private static TodoDAO instance;
+    private List<TodoDTO> list;
+
+    public TodoDAO() {
+
+        list = new ArrayList<TodoDTO>();
+        list.add(new TodoDTO(1,"handel"));
+
+
+    }
+
+    public static TodoDAO getInstance() {
+        if (instance == null)
+            instance = new TodoDAO();
+        return instance;
+    }
+
+    public String getTodoById(int id) {
+        for (TodoDTO curr : list) {
+
+            if (curr.getId() == id)
+                return curr.getTodo();
+
+        }
+
+        return "ID eksisterer ikke i listen";
+    }
+
+    public void addElement(TodoDTO elem) {
+        list.add(elem);
+    }
+
+    public List<TodoDTO> getList() {
+        return list;
+    }
+
+    public String getListAsString() {
+        String totalString = "";
+
+        for (TodoDTO elem : list) {
+            totalString = totalString + "{" + elem.getId() + "," + elem.getTodo() + "}";
+        }
+
+        return totalString;
+    }
+
+}
