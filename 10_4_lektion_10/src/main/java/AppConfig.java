7c20fff15c6a0a36b99a6b82ed069afda2379d76
s174255 <Hfe93pzr>
2020-06-09 15:13:04 +0200

    Added introduction to DAO DTO

---
+++ 10_4_lektion_10/src/main/java/AppConfig.java
@@ -0,0 +1,7 @@
+package main.java;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+@ApplicationPath("/rest")
+public class AppConfig extends Application {
+}
---
+++ 10_4_lektion_10/src/main/java/rest/TodoService.java
@@ -0,0 +1,16 @@
+package main.java.rest;
+
+
+import main.java.data.TodoDAO;
+import main.java.data.TodoDTO;
+
+import javax.ws.rs.*;
+
+@Path("todo")
+public class TodoService {
+    @GET
+    public String getTodoList() {
+        TodoDAO todo = TodoDAO.getInstance();
+        return todo.getListAsString();
+    }
+}
